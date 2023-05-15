a9f88b978d7889737fc39ec9cc59c1b43640d3d9
Jhon12z <133507283+Jhon12z@users.noreply.github.com>
2023-05-15 09:11:34 -0500

    Add files via upload

---
+++ Licorera-copy/Poo/Registro.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Registro here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Registro
+{
+ 
+   String nombre;
+   String tele;
+   String direc;
+   String artic;
+   double canti;
+   double pre;
+   double total;
+
+}
